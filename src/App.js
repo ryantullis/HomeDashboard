f403a2d5549d2cef2e5ff6faca724995e5e3a711
ryantullis <tulliscryan@gmail.com>
2021-12-22 19:17:23 -0600

    added webUI's dropdown

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
     <div className="App">
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">RyanTullis.com</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="radarr">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="WebUI's" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/radarr">Radarr</NavDropdown.Item>
+              <NavDropdown.Item href="/sonarr">Sonarr</NavDropdown.Item>
+              <NavDropdown.Item href="/bazarr">Bazarr</NavDropdown.Item>
+              <NavDropdown.Item href="/jackett">Jackett</NavDropdown.Item>
+              <NavDropdown.Item href="/pihole">Pihole</NavDropdown.Item>
+              <NavDropdown.Item href="/tautulli">Tautulli</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
